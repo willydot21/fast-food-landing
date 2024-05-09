428340c3ad444579bea1726c5a66b33f16e2a9e2
willydot21 <willydot21@gmail.com>
2024-05-09 19:31:22 +0200

    footer update.

---
+++ src/components/footer/footer-data.ts
@@ -0,0 +1,24 @@
+const menuSections = ["Home", "Offers", "Service", "About Us"];
+const platforms = ["pinterest", "instagram", "twitter", "facebook"];
+const references = [
+  "Menu",
+  "Quality",
+  "Make a Choice",
+  "Salad With Vegetable",
+  "Fast Delivery",
+  "Susbscribe",
+];
+
+const contactMethods = [
+  "+123 456 789",
+  "Explore",
+  "info@foodish.com",
+  "1245, New York, USA",
+];
+
+export {
+  menuSections,
+  platforms,
+  references,
+  contactMethods
+}
